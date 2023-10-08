bd201287edf8441c59e2c0e76508a614c5d7955a
Rudra Prasad Panda <prasadrudra858@gmail.com>
2023-10-09 00:17:55 +0530

    HTML,CSS,JS file added

---
+++ HTML,JS,CSS/workshop/datatype.js
@@ -0,0 +1,10 @@
+// var name="rudra"
+// console.log(name)
+// {
+//     let name=[23,24]
+//     console.log(name)
+// }
+// console.log
+var arr=[765,653]
+console.log(arr)
+console.log([765,653])
